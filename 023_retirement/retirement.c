f97acb144d9466492c823b624bf3fe3e40da3e75
gl183 <gl183@duke.edu>
2024-09-04 13:44:34 -0400

    completed 023

--- 023_retirement/retirement.c
+++ 023_retirement/retirement.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct _retire_info{
+struct _retire_info{
   int months;
   double contribution;
   double rate_of_return;
-} retire_info;
+};
+typedef struct _retire_info retire_info;
 
 void print_info(int age, double balance) {
   int yr = age / 12;
