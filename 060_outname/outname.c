2e09032c9004f57ba71d67faddff12bb4813cc96
gl183 <gl183@duke.edu>
2024-10-02 12:44:48 -0400

    completed 060

--- 060_outname/outname.c
+++ 060_outname/outname.c
@@ -4,5 +4,12 @@
 #include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
-  //WRITE ME
+  if (NULL == inputName) {
+    return NULL;
+  }
+  size_t name_len = strlen(inputName);
+  char* ans = malloc((name_len+1+7) * sizeof(*ans));
+  strncpy(ans, inputName, name_len);
+  strncpy(ans+name_len, ".counts", 8);
+  return ans;
 }
