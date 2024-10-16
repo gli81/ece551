7c9b15f92adb76989a8ee5c25c0f3149650cfee3
gl183 <gl183@duke.edu>
2024-10-15 20:28:56 -0400

    completed 067

--- 067_vector_2d/vector.cpp
+++ 067_vector_2d/vector.cpp
@@ -5,3 +5,33 @@
 
 /* write your class implementation in this file
  */
+
+
+void Vector2D::initVector(double init_x, double init_y) {
+  x = init_x;
+  y = init_y;
+}
+
+double Vector2D::getMagnitude() const {
+  return std::sqrt(this->x * this->x + this->y * this->y);
+}
+
+Vector2D Vector2D::operator+(const Vector2D& rhs) const {
+  Vector2D ans; // stack allocation
+  ans.initVector(this->x + rhs.x, this->y + rhs.y);
+  return ans;
+}
+
+Vector2D& Vector2D::operator+=(const Vector2D& rhs) {
+  this->x += rhs.x;
+  this->y += rhs.y;
+  return *this;
+}
+
+double Vector2D::dot(const Vector2D& rhs) const {
+  return this->x * rhs.x + this->y * rhs.y;
+}
+
+void Vector2D::print() const {
+  printf("<%.2f, %.2f>", this->x, this->y);
+}
